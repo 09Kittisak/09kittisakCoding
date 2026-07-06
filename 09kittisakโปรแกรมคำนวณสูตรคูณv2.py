0e197fa5a2e49965553325cd3c374590ad832dc5
09Kittisak <26924@bbw.ac.th>
2026-07-06 09:45:47 +0700

    Add files via upload

---
+++ 09kittisakโปรแกรมคำนวณสูตรคูณv2.py
@@ -0,0 +1,6 @@
+print(">>>โปรแกรมคำนวณแม่สูตรคูณ<<<\n")
+for Multiplication_table in range(2, 5):
+    print("\nตารางสูตรคูณของ", Multiplication_table)
+    for i in range(1, 13):
+        print(Multiplication_table, "*", i, "=",Multiplication_table*i)
+print("\nจัดทำโดย นายกิตติศักดิ์ ไทพนา ม.4/4 เลขที่ 9")
